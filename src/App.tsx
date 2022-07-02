bc845ebacf78d5febf90d95b5dd41d5f001a6967
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-03 00:50:40 +0900

    7.9 MotionValues part Three

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
-import { useEffect } from "react";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   width: 100vw;
-  height: 100vh;
+  height: 200vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg, #e09, #d0e);
 `;
 
 const Box = styled(motion.div)`
@@ -21,17 +26,23 @@ const Box = styled(motion.div)`
 `;
 
 function App() {
-  const x = useMotionValue(0);
-  const scale = useTransform(x, [-400, 0, 400], [3, 0.5, 3]);
+  const { scrollYProgress } = useViewportScroll();
 
-  useEffect(() => {
-    // x.onChange(() => console.log(x.get()));
-    scale.onChange(() => console.log(scale.get()));
-  }, [x]);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-400, 400], [-720, 720]);
+  const gradient = useTransform(
+    x,
+    [-400, 400],
+    [
+      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
+    ]
+  );
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   return (
-    <Wrapper>
-      <Box style={{ x, scale }} drag="x" dragSnapToOrigin />
+    <Wrapper style={{ background: gradient }}>
+      <Box style={{ x, scale, rotateZ }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
