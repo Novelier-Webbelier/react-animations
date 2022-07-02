d37f73f32c929b467f4088e279ebc5d6e02ed627
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-03 00:03:21 +0900

    7.6 Gestures part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -10,13 +11,23 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   background-color: white;
-  border-radius: 40px;
+  border-radius: 30px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const BiggerBox = styled(motion.div)`
+  width: 400px;
+  height: 400px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const boxVariants = {
@@ -38,15 +49,22 @@ const boxVariants = {
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag="drag"
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.2}
+          dragConstraints={biggerBoxRef}
+          variants={boxVariants}
+          whileHover="hover"
+          whileDrag="drag"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
