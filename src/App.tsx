d7413c2ad538c3f8dbfc58ba206c1c871bbc5049
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-03 00:13:55 +0900

    7.7 MotionValues part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -20,51 +20,17 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const BiggerBox = styled(motion.div)`
-  width: 400px;
-  height: 400px;
-  background-color: rgba(255, 255, 255, 0.2);
-  border-radius: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const boxVariants = {
-  hover: {
-    scale: 1.5,
-    rotateZ: 90,
-  },
-  click: {
-    scale: 1,
-    borderRadius: "100px",
-  },
-  drag: {
-    backgroundColor: "rgba(46, 204, 113, 1)",
-    transition: {
-      type: "spring",
-      bounce: 0.78,
-    },
-  },
-};
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+
+  useEffect(() => {
+    x.onChange(() => console.log(x.get()));
+  }, [x]);
 
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.2}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileDrag="drag"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <button onClick={() => x.set(200)}>click me</button>
+      <Box style={{ x }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
