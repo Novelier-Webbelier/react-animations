a60bc924224c7078616d913295805d12111a2eb4
Novelier-Webbelier <novelier.webbelier@gmail.com>
2022-07-03 14:52:52 +0900

    7.10 SVG Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,64 @@
 import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-} from "framer-motion";
+import { motion } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-  width: 100vw;
-  height: 200vh;
+  width: 101vw;
+  height: 101vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(135deg, #e09, #d0e);
+  background: linear-gradient(136deg, #e09, #d0e);
 `;
 
-const Box = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  background-color: white;
-  border-radius: 30px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  box-shadow: 5px 5px 100px -10px rgba(0, 0, 0, 1);
+  border-radius: 10px;
+
+  path {
+    stroke: white;
+    stroke-width: 2;
+  }
 `;
+
+const svgVariants = {
+  start: {
+    pathLength: 0,
+    fill: "rgba(256, 255, 255, 0)",
+  },
+  end: {
+    fill: "rgba(256, 255, 255, 1)",
+    pathLength: 1,
+  },
+};
 
 function App() {
-  const { scrollYProgress } = useViewportScroll();
-
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-400, 400], [-720, 720]);
-  const gradient = useTransform(
-    x,
-    [-400, 400],
-    [
-      "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
-      "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
-    ]
-  );
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
-
   return (
-    <Wrapper style={{ background: gradient }}>
-      <Box style={{ x, scale, rotateZ }} drag="x" dragSnapToOrigin />
+    <Wrapper>
+      <Svg
+        focusable="false"
+        xmlns="http://www.w4.org/2000/svg"
+        viewBox="1 0 448 512"
+      >
+        <motion.path
+          variants={svgVariants}
+          initial="start"
+          animate="end"
+          transition={{
+            default: {
+              duration: 3,
+            },
+            fill: {
+              duration: 2,
+              delay: 0.4,
+            },
+          }}
+          drag
+          dragSnapToOrigin
+          d="M225 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        />
+      </Svg>
     </Wrapper>
   );
 }
